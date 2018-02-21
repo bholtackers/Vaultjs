11ce27ed85d46de58ba04498f900a098fe523f2b
Bram <bholtackers@outlook.com>
2018-02-21 13:09:49 +0100

    Fade animation added and blinking added(STILL WIP)

--- js/main.js
+++ js/main.js
@@ -2,9 +2,12 @@ var codeContainer = document.getElementById('Entered-Code');
 var ButtonDisabler = document.getElementsByClassName('Button')
 var redLight = document.getElementById('Light-Red')
 var greenLight = document.getElementById('Light-Green')
+var outcome = document.getElementById('Outcome')
 var CorrectCode = "123";
 var CorrectTimes = 0;
 var IncorrectTimes = 0;
+var Ended = false;
+var blinkCounter = 0;
 
 function getNumber(button)
 {
@@ -13,45 +16,83 @@ function getNumber(button)
   var DisableCounter = 0;
   for (DisableCounter = 0; DisableCounter < ButtonDisabler.length; DisableCounter++)
     {
-      if (codeLengthChecker == 3)
+      if (codeLengthChecker === 3)
         {
           ButtonDisabler[DisableCounter].disabled = true;
-          setTimeout(resetButton,10000)
+          setTimeout(resetButton,6000)
         }
       else
         {
           ButtonDisabler[DisableCounter].disabled = false;
         }
     }
-  if (codeContainer.innerHTML == CorrectCode)
+  if (codeContainer.innerHTML === CorrectCode)
     {
-      document.getElementById('Outcome').innerHTML = "THE CODE IS CORRECT"
+      Ended = true;
+      outcome.classList.add("fade-in");
+      outcome.innerHTML = "THE CODE IS CORRECT"
+        toggle_color("rgb(0, 74, 7)", "rgb(0, 199,19)", 1000, 500);
+        function toggle_color(color1, color2, cycle_time, wait_time) {
+
+            setInterval(function first_color() {
+                greenLight.style.backgroundColor = color1;
+                setTimeout(change_color, wait_time);
+            }, cycle_time);
+
+            function change_color() {
+                greenLight.style.backgroundColor = color2;
+            }
+        }
+
       CorrectTimes++;
       greenLight.innerHTML = "CORRECT: " + CorrectTimes.toString() + " TIMES"
       var audio = new Audio('snd/Open.mp3');
       audio.play();
-      setTimeout(reset, 10000)
+      setTimeout(reset, 6000)
     }
-else if ((codeContainer.innerHTML != CorrectCode) && (codeLengthChecker == 3) )
+  else if ((codeContainer.innerHTML != CorrectCode) && (codeLengthChecker === 3) )
     {
-      document.getElementById('Outcome').innerHTML = "THE CODE IS INCORRECT"
+      Ended = true;
+      outcome.classList.add("fade-in");
+      outcome.innerHTML = "THE CODE IS INCORRECT"
+        toggle_color("rgb(74, 7, 0)", "rgb(199, 19, 0)", 1000, 500);
+        function toggle_color(color1, color2, cycle_time, wait_time) {
+
+            setInterval(function first_color() {
+                blinkCounter++;
+                redLight.style.backgroundColor = color1;
+                setTimeout(change_color, wait_time);
+            }, cycle_time);
+
+            function change_color() {
+                redLight.style.backgroundColor = color2;
+            }
+        }
+
       IncorrectTimes++;
       redLight.innerHTML = "INCORRECT: " + IncorrectTimes.toString() + " TIMES"
       var audio = new Audio('snd/Close.mp3');
       audio.play();
-      setTimeout(reset, 10000)
+      setTimeout(reset, 6000)
     }
 }
 
 function reset(){
   DisableCounter = 0;
+  Ended = false;
+  blinkCounter = 0;
   codeContainer.innerHTML = "";
-  document.getElementById('Outcome').innerHTML = "";
+  outcome.innerHTML = "";
+  outcome.classList.remove("fade-in");
+  greenLight.style.backgroundColor = "rgb(0, 74, 7)";
 }
 
 function resetButton(){
   DisableCounter = 0;
   ButtonDisabler[DisableCounter].disabled = false;
 }
+
+
+
 
 //Blinker kleuren: "rgb(199, 19, 0)" en "rgb(0, 199,19)"
